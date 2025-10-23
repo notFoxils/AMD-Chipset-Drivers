ae68c862d06d4e49fbd53044af2519376ffb5197
notFoxils <voltrixz9514@gmail.com>
2025-10-23 19:32:46 -0400

    Feat: Default to help when running without a command specified

--- src/amd_chipset_driver_scraper/command/__init__.py
+++ src/amd_chipset_driver_scraper/command/__init__.py
@@ -3,7 +3,7 @@ import typer
 from .check_for_new_driver import app as check_for_new_driver_app
 from .download import app as download_app
 
-app: typer.Typer = typer.Typer()
+app: typer.Typer = typer.Typer(no_args_is_help=True)
 app.add_typer(check_for_new_driver_app)
 app.add_typer(download_app)
 
